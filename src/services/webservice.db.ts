cdbfb9d6ea0ebb225d2edc8f0bf606818c8cb02e
Divyanshu Tiwari <dtiwari@securonix.com>
2023-08-23 00:34:29 +0530

    saving the db type as postgres

--- src/services/webservice.db.ts
+++ src/services/webservice.db.ts
@@ -8,7 +8,7 @@ const LOGGER = new Log("Main").logger
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "postgres",
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
